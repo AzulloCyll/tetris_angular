b8aa74d9598262c697cc4511dee5249b87788665
Daniel Chmur <daniel.chmur@gmail.com>
2022-01-05 08:33:44 +0100

    tabs conditional rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngZal';
+  showPage = 'intro';
+  whichActive = 'intro';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { IntroComponent } from './intro/intro.component';
 import { GameComponent } from './game/game.component';
 
